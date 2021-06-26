0c88ff2ed7d2dc6e28084d2a922dc10e08b36924
supergalou <gael.joliveau@icloud.com>
2021-06-26 19:41:46 +0200

    feat: add elements to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import BookService from "../services/Book.service";
 import '../assets/css/Home.css';
 import Header from "../components/Header";
+import reading from '../assets/img/reading.svg';
 
 const Home = () => {
 
@@ -20,8 +21,25 @@ const Home = () => {
   return (
     <div className="page-container">
       <Header />
-      <p>La librairie toujour ouverte !</p>
-      <p>Trouve ton livre d'Henri Potier avec nous</p>
+      <div className="inline">
+        <div>
+          <p className="catchphrase"> La librairie toujours ouverte !</p>
+          <p className="catchphrase">Trouve ton livre d'Henri Potier au meilleur prix avec nous</p>
+        </div>
+        <img className="illustration" src={reading} alt="pannier" />
+      </div>
+
+      <div className="inline cards-container">
+        {books.map((book)=>{
+          return(
+            <div className="card">
+              <img src={book.cover} className="book-cover" alt={`couverture du livre ${book.title}`}/>
+              <p>{book.title}</p>
+            </div>
+          );
+        })}
+      </div>
+
     </div>
   );
 };
