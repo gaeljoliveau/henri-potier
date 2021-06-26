705f1509d4bc779f290ae202059b539a9b9b2a1a
supergalou <gael.joliveau@icloud.com>
2021-06-26 20:12:56 +0200

    feat: add header to 404

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import blibrarylogo from '../assets/img/Blibrary.png';
 import '../assets/css/components/Header.css';
 import Basket from '../assets/img/basket.svg';
 
+
 const Header = () => {
   return (
     <div className="header-wrapper inline">
-      <div className="inline">
+      <Link className="inline" to="/">
         <img className="header-logo" src={blibrarylogo} alt="Logo" />
         <p className="header-title" >Blibrary</p>
-      </div>
+      </Link>
 
       <div className="inline bloc-right">
         <p className="text" >Pannier</p>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
   return(
-    <p>Error 404 : Page Not Found</p>
+    <div className="page-container">
+      <Header />
+      <p>Error 404 : Page Not Found</p>
+    </div>
+    
   );
 };
 
